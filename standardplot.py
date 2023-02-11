e2ae39de302e8c1b8661b0be5eaac4d53e49569f
Victor <quintv2@unlv.nevada.edu>
2023-02-10 23:11:30 -0800

    added features

--- standardplot.py
+++ standardplot.py
@@ -3,6 +3,7 @@ import numpy as np
 y = np.linspace(0,20,5)
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 XX, YY = np.meshgrid(x,y)
+plt.plot(x,np.sin(x))
 #plt.plot(x, np.sin(x))       # Plot the sine of each x point
 #plt.show()                   # Display the plot
 
